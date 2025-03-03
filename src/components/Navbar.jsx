3caf3185ac2df90d0744384bd5f47cf1a7af41c3
Nkugavarathan <kugavarathan28@gmail.com>
2025-03-03 11:36:38 +0530

    react router dom pacakage installed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,17 @@
-import React from "react"
-
-export default function Navbar() {
-  return <div>Navbar</div>
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="path" className="navbar-logo">
+            TRVL <i className="fab fa-typo3"></i>
+          </Link>
+        </div>
+      </nav>
+    </>
+  )
 }
+
+export default Navbar
